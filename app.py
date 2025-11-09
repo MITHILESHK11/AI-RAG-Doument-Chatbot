a15f651d2a21027f32e8ceefd93b32d7c3e73f3e
Kolhapurkar Mithilesh Yogesh <112339988+MITHILESHK11@users.noreply.github.com>
2025-11-09 18:23:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ from langchain_groq import ChatGroq
 from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import FAISS
-from langchain_community.storage import InMemoryStore
+from langchain.storage.memory import InMemoryStore
 from langchain_community.retrievers.multi_vector import MultiVectorRetriever
 import pandas as pd
 import google.generativeai as genai
